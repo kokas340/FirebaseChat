dcfbff3f1cdcdb830f1b63f0ce4ef014db2af87b
Jack Spinola <65230037+kokas340@users.noreply.github.com>
2022-11-06 16:16:18 +0100

    firebase

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDHQuTnhcWx4ZazhFpKKs6HGLXceJ4H43w",
+    authDomain: "nightlife-7e64b.firebaseapp.com",
+    projectId: "nightlife-7e64b",
+    storageBucket: "nightlife-7e64b.appspot.com",
+    messagingSenderId: "76508944337",
+    appId: "1:76508944337:web:bc32809c1b322bb35272f0"
+  };
