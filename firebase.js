121510f24c95f9379428e9edb86f2aedce98af0e
Jack Spinola <65230037+kokas340@users.noreply.github.com>
2023-07-31 09:01:54 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,7 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDHQuTnhcWx4ZazhFpKKs6HGLXceJ4H43w",
-    authDomain: "nightlife-7e64b.firebaseapp.com",
-    projectId: "nightlife-7e64b",
-    storageBucket: "nightlife-7e64b.appspot.com",
-    messagingSenderId: "76508944337",
-    appId: "1:76508944337:web:bc32809c1b322bb35272f0"
+ 
   };
  
   const app=firebase.initializeApp(firebaseConfig);
